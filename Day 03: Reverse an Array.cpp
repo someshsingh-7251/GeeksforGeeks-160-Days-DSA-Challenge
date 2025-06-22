20e19663a00d37a4c70eb3863f8d71a664cd0ead
Somesh Singh <somesh572000@gmail.com>
2025-06-22 11:52:42 +0530

    Day 03: Reverse an Array.cpp

---
+++ Day 03: Reverse an Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        int s=0;
+        int e=arr.size()-1;
+        while(s<e){
+            swap(arr[s],arr[e]);
+            s++;
+            e--;
+        }
+    }
+};
